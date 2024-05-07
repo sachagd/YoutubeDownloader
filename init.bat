0bfa02c30797b112607564d9bc663e9fdb209097
Sacha <106993611+sachagd@users.noreply.github.com>
2024-05-08 01:44:26 +0200

    Add files via upload

--- init.bat
+++ init.bat
@@ -1,5 +1,4 @@
 @echo off
-SETLOCAL EnableExtensions
 
 :: Checking if pip is available
 python -m pip --version >nul 2>&1
@@ -18,12 +17,14 @@ if %ERRORLEVEL% neq 0 (
         :: Check if pip installed successfully
         python -m pip --version >nul 2>&1
         if %ERRORLEVEL% neq 0 (
-            echo Failed to install pip. Please check your Python installation.
+            echo Failed to install pip.
+            pause
             goto end
         )
         echo pip installed successfully.
     ) else (
-        echo pip installation was skipped by user. Exiting setup.
+        echo Installation skipped.
+        pause
         goto end
     )
 )
@@ -31,7 +32,7 @@ if %ERRORLEVEL% neq 0 (
 echo Checking and installing required Python libraries...
 
 :: Set the libraries to install
-set "LIBRARIES=pytube beautifulsoup4 requests"
+set "LIBRARIES=tk pytube beautifulsoup4 requests"
 
 :: Install each library
 for %%i in (%LIBRARIES%) do (
@@ -48,19 +49,46 @@ for %%i in (%LIBRARIES%) do (
 echo All required libraries have been checked and installed if needed.
 
 :: Add the registry key
-echo Adding the registry key for the Firefox extension...
-set "REG_PATH=HKCU\Software\Mozilla\NativeMessagingHosts\com.sacha.youtubedownloader"
-set "DIR=%~dp0"
-set "JSON_PATH=%DIR:~0,-1%\native-messaging.json"
-reg add "%REG_PATH%" /ve /t REG_SZ /d "%JSON_PATH%" /f
+    echo Adding the registry key for the Firefox extension...
+    set "REG_PATH=HKCU\Software\Mozilla\NativeMessagingHosts\com.sacha.youtubedownloader"
+    set "DIR=%~dp0"
+    set "JSON_PATH=%DIR:~0,-1%\native-messaging.json"
+    reg add "%REG_PATH%" /ve /t REG_SZ /d "%JSON_PATH%" /f
 
 if %ERRORLEVEL% == 0 (
     echo Registry key added successfully.
 ) else (
     echo Failed to add registry key.
+    pause
+    goto end
 )
 
-:end
+ffmpeg -version >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    echo ffmpeg is already installed.
+    pause
+    goto end
+)
+
+echo ffmpeg is not installed.
+echo ffmpeg is necessary to run the extension
+SET /P user_input="Do you want to download and install ffmpeg? [Y/N] "
+IF /I NOT "%user_input%"=="Y" (
+    echo Installation skipped
+    pause
+    goto end
+)
+echo Downloading and extracting ffmpeg...
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip' -OutFile '%ProgramFiles%\ffmpeg-master-latest-win64-gpl.zip'; Expand-Archive -Path '%ProgramFiles%\ffmpeg-master-latest-win64-gpl.zip' -DestinationPath '%ProgramFiles%\ffmpeg' -Force"
+
+del "%ProgramFiles%\ffmpeg-master-latest-win64-gpl.zip"
+
+:: Add FFmpeg bin to PATH
+SET "ffmpeg_bin=%ProgramFiles%\ffmpeg\ffmpeg-master-latest-win64-gpl\bin"
+SETX PATH "%PATH%;%ffmpeg_bin%" -m
+
+echo ffmpeg installed successfully.
+
 pause
-ENDLOCAL
 start /b "" cmd /c del "%~f0"&exit /b
+:end
