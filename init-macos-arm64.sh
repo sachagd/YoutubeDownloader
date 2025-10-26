7964ac989cd9954b87c22f000eff5fc586194160
Sachagd <sach4gd@gmail.com>
2025-10-26 18:15:53 +0100

    small changes

--- init-macos-arm64.sh
+++ init-macos-arm64.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "$0")" && pwd)"
+sed -i '' "s|APP_PATH|$DIR/ytdl-macos-arm64|g" "$DIR/native-messaging.json"
 mkdir -p "$HOME/Library/Application Support/Mozilla/NativeMessagingHosts"
 cp "$DIR/native-messaging.json" "$HOME/Library/Application Support/Mozilla/NativeMessagingHosts/com.sacha.youtubedownloader.json"
 echo "Native messaging host registered successfully."
-chmod +x ytdl_macos_arm64
+chmod +x ytdl-macos-arm64
+xattr -d com.apple.quarantine "$DIR/ytdl-macos-arm64"
+xattr -dr com.apple.quarantine "$DIR/_internal"
 rm -- "$0"
--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "$0")" && pwd)"
+sed -i "s|APP_PATH|$DIR/ytdl-ubuntu|g" "$DIR/native-messaging.json"
 mkdir -p "$HOME/.mozilla/native-messaging-hosts"
 cp "$DIR/native-messaging.json" "$HOME/.mozilla/native-messaging-hosts/com.sacha.youtubedownloader.json"
 echo "Native messaging host registered successfully."
